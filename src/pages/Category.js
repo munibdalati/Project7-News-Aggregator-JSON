be8a154a0d57ff18e5bef0b03cedfa5f7713d956
unknown <suha.a.badarneh@gmail.com>
2023-06-22 15:53:36 +0300

    make category filtering

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -5,6 +5,7 @@ import Col from "react-bootstrap/Col";
 import NewCard from "../component/NewCard";
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { AllNews } from "../newsData";
 
 let news = [
   {
@@ -175,58 +176,97 @@ let news = [
 const Category = () => {
   const params = useParams();
   const props = params.type;
+  const filteredObjectsSport = AllNews.filter((obj) => obj.type === "sport");
+  const filteredObjectsEconomy = AllNews.filter(
+    (obj) => obj.type === "economy"
+  );
+  const filteredObjectsBreaking = AllNews.filter(
+    (obj) => obj.type === "breaking"
+  );
+  const filteredObjectsWorld = AllNews.filter((obj) => obj.type === "world");
+  const filteredObjectsHealth = AllNews.filter((obj) => obj.type === "health");
+
   return (
     <Container>
       <Row>
         {props === "sport" && (
-          <p
-            className="category-title mx-auto mt-5 py-1 px-2 text-white"
-            style={{ backgroundColor: "#ff8d00" }}
-          >
-            أخبار الرياضة
-          </p>
+          <>
+            <p
+              className="category-title mx-auto mt-5 py-1 px-2 text-white"
+              style={{ backgroundColor: "#ff8d00" }}
+            >
+              أخبار الرياضة
+            </p>
+            <Row className="d-flex justify-content-center gap-4 my-5">
+              {filteredObjectsSport.map((obj) => (
+                <NewCard data={obj} />
+              ))}
+            </Row>
+          </>
         )}
 
         {props === "economy" && (
-          <p
-            className="category-title mx-auto mt-5 py-1 px-2 text-white"
-            style={{ backgroundColor: "#17c8bd" }}
-          >
-            أخبار الاقتصاد
-          </p>
+          <>
+            <p
+              className="category-title mx-auto mt-5 py-1 px-2 text-white"
+              style={{ backgroundColor: "#17c8bd" }}
+            >
+              أخبار الاقتصاد
+            </p>
+            <Row className="d-flex justify-content-center gap-4 my-5">
+              {filteredObjectsEconomy.map((obj) => (
+                <NewCard data={obj} />
+              ))}
+            </Row>
+          </>
         )}
-
         {props === "breaking" && (
-          <p
-            className="category-title mx-auto mt-5 py-1 px-2 text-white"
-            style={{ backgroundColor: "#FF0000" }}
-          >
-            عاجل
-          </p>
+          <>
+            <p
+              className="category-title mx-auto mt-5 py-1 px-2 text-white"
+              style={{ backgroundColor: "#FF0000" }}
+            >
+              عاجل
+            </p>
+            <Row className="d-flex justify-content-center gap-4 my-5">
+              {filteredObjectsBreaking.map((obj) => (
+                <NewCard data={obj} />
+              ))}
+            </Row>
+          </>
         )}
 
         {props === "world" && (
-          <p
-            className="category-title mx-auto mt-5 py-1 px-2 text-white"
-            style={{ backgroundColor: "#9933CC" }}
-          >
-            العالم
-          </p>
+          <>
+            <p
+              className="category-title mx-auto mt-5 py-1 px-2 text-white"
+              style={{ backgroundColor: "#9933CC" }}
+            >
+              العالم
+            </p>
+            <Row className="d-flex justify-content-center gap-4 my-5">
+              {filteredObjectsWorld.map((obj) => (
+                <NewCard data={obj} />
+              ))}
+            </Row>
+          </>
         )}
-        {props === "health" && (
-          <p
-            className="category-title mx-auto mt-5 py-1 px-2 text-white"
-            style={{ backgroundColor: "#3366CC " }}
-          >
-            صحة
-          </p>
-        )}
-      </Row>
 
-      <Row className="d-flex justify-content-center gap-4 my-5">
-        {news.map((newsItem) => {
-          return <NewCard data={newsItem} />;
-        })}
+        {props === "health" && (
+          <>
+            <p
+              className="category-title mx-auto mt-5 py-1 px-2 text-white"
+              style={{ backgroundColor: "#3366CC" }}
+            >
+              العالم
+            </p>
+            <Row className="d-flex justify-content-center gap-4 my-5">
+              {filteredObjectsHealth.map((obj) => (
+                <NewCard data={obj} />
+              ))}
+            </Row>
+          </>
+        )}
       </Row>
 
       <Row>
@@ -238,7 +278,7 @@ const Category = () => {
       </Row>
 
       <Row className="d-flex justify-content-center gap-4 my-5">
-        {news.map((newsItem) => {
+        {AllNews.map((newsItem) => {
           return <NewCard data={newsItem} />;
         })}
       </Row>
