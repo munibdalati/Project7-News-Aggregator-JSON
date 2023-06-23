a7e3aab2afde01076e67adbe7676d9c9feea0939
unknown <suha.a.badarneh@gmail.com>
2023-06-23 23:41:58 +0300

    edit calendar style

--- src/component/Calender.js
+++ src/component/Calender.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import "../assets/index.css";
+import "../assets/calendar.css";
 
 function Calender() {
   const [value, onChange] = useState(new Date());
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} className="" />
+      <Calendar
+        className="calendar-card"
+        style={{ width: "20rem" }}
+        onChange={onChange}
+        value={value}
+      />
     </div>
   );
 }
