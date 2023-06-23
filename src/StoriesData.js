7033f155be44b1e218466235784a90097e9479e1
munib dalati <munib.dalati@gmail.com>
2023-06-23 22:26:52 +0300

    adding all StoriesData

--- src/StoriesData.js
+++ src/StoriesData.js
@@ -1,3 +1,15 @@
-const StoriesData = [
-    {name : "خبرني", image: "https://pbs.twimg.com/media/Fy-FHr0aUAET757.jpg" }
+export const StoriesData = [
+    {name : "خبرني", image: "https://pbs.twimg.com/media/Fy-FHr0aUAET757.jpg" },
+    {name : "السوسنة", image: "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243142.jpg" },
+    {name : "الوكيل", image:       "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/x_0,y_0,w_5504,h_3097,c_crop/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243145.jpg",
+},
+    {name : "الدستور", image:       "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/x_0,y_0,w_5504,h_3097,c_crop/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243145.jpg",
+},
+    {name : "رؤيا", image: "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/x_0,y_0,w_5276,h_2967,c_crop/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243139.jpg" },
+    {name : "عمون", image: "https://img.alwakeelnews.com/Content/Upload/large/620232294511309129216.jpg" },
+    {name : "روسيا اليوم", image: "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/x_0,y_0,w_3000,h_1687,c_crop/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243136.jpg" },
+    {name : "العربية", image: "https://www.ngmisr.com/wp-content/uploads/2022/08/ameca-article-thumbnail-1200x800-1.jpg" },
+    {name : "الجزيرة", image: "https://www.khaberni.com/uploads/news_model/2023/06/main_image6493f212532c0.jpg" },
+    {name : "أورينت", image: "https://cnn-arabic-images.cnn.io/cloudinary/image/upload/x_0,y_118,w_4993,h_2808,c_crop/w_1920,h_1008,c_fill,q_auto,g_center/t_cnnarabic_watermark_logo_v5/cnnarabic/2023/06/19/images/243132.jpg" },
+    {name : "حلب اليوم", image: "https://images.skynewsarabia.com/images/v1/2019/05/16/1252563/1920/1080/1-1252563.jpg" }
 ]
